9db230944613c0341868991cc8b98874331e9bca
beplaya <beplaya@gmail.com>
2018-03-15 23:07:17 -0400

    it_gets_hours_of_work_after_bedtime

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,19 @@ impl Babysitter {
         }
         return hours;
     }
+
+
+    fn get_hours_after_bedtime(&self, arrivalTime: i32, departureTime: i32, bedtime: i32) -> i32 {
+        let hours : i32;
+        if arrivalTime == departureTime {
+            hours = 0;
+        } else if departureTime < 5 {
+            hours = (12 - bedtime) + departureTime;
+        } else {
+            hours = departureTime - bedtime;
+        }
+        return hours;
+    }
 }
 
 
@@ -42,6 +55,15 @@ mod babysitter_tests {
         assert_eq!(babysitter.get_hours_before_bedtime(4, 4, 12), 0);
         assert_eq!(babysitter.get_hours_before_bedtime(1, 4, 12), 0);
         assert_eq!(babysitter.get_hours_before_bedtime(5, 4, 5), 0);
+    }
+
+    #[test]
+    fn it_gets_hours_of_work_after_bedtime() {
+        let babysitter = Babysitter {};
+        assert_eq!(babysitter.get_hours_after_bedtime(5, 12, 8), 4);
+        assert_eq!(babysitter.get_hours_after_bedtime(4, 4, 12), 0);
+        assert_eq!(babysitter.get_hours_after_bedtime(1, 4, 12), 4);
+        assert_eq!(babysitter.get_hours_after_bedtime(5, 4, 5), (12 - 5) + 4);
     }
 
 
